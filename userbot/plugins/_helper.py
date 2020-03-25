64839e4e8a139724b568584509fc5140358f0044
HACKERZKALI <62069925+HACKERZKALI@users.noreply.github.com>
2020-03-25 19:29:18 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -37,7 +37,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper.. Provided by @TODAYSHACKER \n [Check out my sir Channel](https://t.me/eonarmy) \n
+            help_string = """Userbot Helper.. Provided by @TODAYSHACKER \n [Check out my sir Channel](https://t.me/netflix_commix) \n
 `Sir Helper to reveal all the commands`"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
